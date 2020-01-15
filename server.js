e5c95cf8bd48172417dbed6b97e5274b654a289c
kyllo34 <kyllo34@gmail.com>
2020-01-14 17:26:05 -0800

    completed feature 2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('dotenv')
 const express = require('express');
 const app = express();
 require('dotenv').config();
@@ -18,12 +19,33 @@ app.get('/location', (request, response) => {
   response.status(200).send(location);
 });
 
+
 function Location(city, locationData) {
   this.search_query = city;
   this.formatted_query = locationData.display_name;
   this.latitude = locationData.lat;
   this.longitude = locationData.lon
 }
+const dailySummaries = [];
+app.get('/weather', (request, response) => {
+  let city = request.query.city;
+  const geoWeather = require('./data/darksky.json');
+  geoWeather.daily.data.forEach(day => {
+    dailySummaries.push(new DailySummary(day));
+  });
+
+  response.status(200).send(dailySummaries);
+
+})
+
+function DailySummary(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0,15);
+  dailySummaries.push(this);
+
+}
+
+
 
 
 app.listen(PORT, () => {
